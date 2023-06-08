962030e834c5431b09ba422c7171b205ecc9a1b4
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-08 15:25:53 -0700

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-#imports function defiition and applies it
-a = 1
-b = 2
+    # Imports function definition and applies it
+    a = 1
+    b = 2
 
-from add_0 import add
+    from add_0 import add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
