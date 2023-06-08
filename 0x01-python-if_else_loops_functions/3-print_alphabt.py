593cb4bbbee6d8c186d9cec85ac15996b14a71cd
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-07 19:26:43 -0700

    updt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 #gives the following output abcdfghijklmnoprstuvwxyz
 for letter in range(97, 123):
-    if chr(letter) not in ['q', 'e']:
-        #if statement to eliminate the two characters
+    if chr(letter) is not ['q', 'e']:
         print("{}".format(chr(letter)), end="")
