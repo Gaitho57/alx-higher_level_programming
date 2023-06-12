95eae668b57cd199267d3d9375f4caa4a85ab5b5
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 16:28:15 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+# 8-multiple_returns.py
 
 def multiple_returns(sentence):
-    # Check if the sentence is empty
     if sentence == "":
-        # If the sentence is empty, return a tuple with 0 and None
-        return 0, None
-
-    # Return a tuple with the length of the sentence and its first character
-    return len(sentence), sentence[0]
-
+        return (0, None)
+    return (len(sentence), sentence[0])
