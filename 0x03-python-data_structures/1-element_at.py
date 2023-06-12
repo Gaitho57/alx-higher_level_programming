d4219702e4f9021c94f858443b7c0adf9bc561cf
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 15:39:52 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 # 1-element_at.py
 
 def element_at(my_list, idx):
-    # Check if the index is out of bounds
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >(len(my_list) - 1):
         return None
-
     return my_list[idx]
 
