5b250a53e8d7eb42959921815758807af2f79c44
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 16:12:21 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-def no_c(my_string):
-    # Create a new list by filtering out characters 'c' and 'C' from the original string
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-
-    # Join the filtered characters from the list to form a new string
-    new_string = "".join(copy)
-
-    # Return the new string with 'c' and 'C' removed
-    return new_string
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
 
