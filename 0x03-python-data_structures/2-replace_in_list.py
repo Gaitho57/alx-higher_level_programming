25d67cf15c53d6be2f5a440363e4e262c3a64195
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 15:51:13 -0700

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
-
 def replace_in_list(my_list, idx, element):
-    # Check if the index is within the bounds of the list
     if idx >= 0 and idx < len(my_list):
-        # Replace the element at the specified index with the new element
         my_list[idx] = element
-
-    # Return the modified list
-    return my_list
-
+    return (my_list)
