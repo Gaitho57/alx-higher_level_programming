b1ff3840484de0e446031c67ccf5777a2749613d
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 16:07:21 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
+# 5-no_c.py
 
 def no_c(my_string):
-    # Create a new list by filtering out characters 'c' and 'C' from the original string
     copy = [x for x in my_string if x != 'c' and x != 'C']
-
-    # Join the filtered characters from the list to form a new string
-    new_string = "".join(copy)
-
-    # Return the new string with 'c' and 'C' removed
-    return new_string
-
+    return ("".join(copy))
