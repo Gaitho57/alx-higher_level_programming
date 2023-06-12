262f6239b878524bcfabf05db731186cb90f4417
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-12 16:51:05 -0700

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-
-def delete_at(my_list=[], idx=0):
-    # Check if the given index is within the valid range for the list
-    if idx >= 0 and idx < len(my_list):
-        # If the index is valid, delete the element at the specified index using the 'del' statement
-        del my_list[idx]
-
-    # Return the modified list
-    return my_list
+def divisible_by_2(my_list=[]):
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+    return multiples
 
