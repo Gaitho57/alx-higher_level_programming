22266e0084e144d7eb717df8caf7f0d9eead8938
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-20 04:44:46 -0700

    print_comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for number in range(100):
-    if number != 99:
-print("{:02d}, ".format(number), end="")
-    else:
-print("{:02d}".format(number))
 
+# Print numbers from 0 - 99 with leading zeros
+for num in range(0, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
