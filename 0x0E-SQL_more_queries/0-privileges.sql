d79ab796dd9e79196cb674d4e9d46b2a0a5cd67f
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-17 15:03:15 -0700

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- script lists all privileges of the MySQL users
+-- script that lists all privileges of the MySQL users
 -- user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- lists privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
