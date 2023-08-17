24ebf990657b88b76a0487510423476e7254d44d
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-17 15:08:11 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- makes a user and sets the password
 -- creates user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
--- grant permission
-GRANT ALL PRIVILEGES ON *.*
-TO 'user_0d_1'@'localhost';
+-- a script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
