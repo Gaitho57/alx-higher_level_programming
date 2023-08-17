6e589502c0a64d0f2cc65149f4a7c842c24b8f1d
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-17 12:19:47 -0700

    add

--- 0x0D-SQL_introduction/0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,9 @@
--- script that lists all databases of MySQL server
--- Query to display databases
-SHOW DATABASES;
+#!/bin/bash
+
+# MySQL server connection details
+MYSQL_USER="root"
+MYSQL_PASSWORD="your_mysql_password"
+MYSQL_HOST="localhost"
+
+# Execute the command to list databases
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -e "SHOW DATABASES;"
