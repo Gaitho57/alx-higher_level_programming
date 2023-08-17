ee559ea71663e6085c63a0ad1fe513f0b7f9e757
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-17 12:27:31 -0700

    list

--- 0x0D-SQL_introduction/0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0x0D-SQL_introduction/0-list_databases.sql
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-# MySQL server connection details
-MYSQL_USER="root"
-MYSQL_PASSWORD="your_mysql_password"
-MYSQL_HOST="localhost"
-
-# Execute the command to list databases
-mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -e "SHOW DATABASES;"
+-- Query to display databases
+SHOW DATABASES;
