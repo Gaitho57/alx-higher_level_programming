53c9da8a2fe0b092dc4785af94d2a2d1b65ce38c
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-13 17:18:58 +0300

    contact_update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+
+const arguments = process.argv.slice(2);
+
+let FirstVar = arguments[0];
+let SecondVar = arguments[1];
+
+FirstVar || 'undefined';
+SecondVar || 'undefined';
+
+console.log(`{FirstVar} is {SecondVar}`);
