c647bbcdb385e6d81374120be089f24fa9728541
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-13 13:58:53 +0300

    value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arguments = process.arv.slice(2);
+const arguments = process.argv.slice(2);
 
-if (arguments[0] === 0){
-    console.log("No argument")
+if (!arguments[0]){
+    console.log("No argument");
 }
 else{
     console.log(arguments[0]);
