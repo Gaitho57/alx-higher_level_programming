4ff79f865862e8b1e3933e2c2c1d636c1be01ac9
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-13 18:34:47 +0300

    multi_c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,12 @@
 
 const args = process.argv.slice(2);
 
-const parse = parseInt(args[0]);
+const x = parseInt(args[0]);
 
-if (isNaN(parse)) {
+if (isNaN(x) || x === 'undefined') {
     console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < parse; i++) {
+    for (let i = 0; i < x; i++) {
         console.log("C is fun");
     }
 }
