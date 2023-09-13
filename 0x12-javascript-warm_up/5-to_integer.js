621894f8c01bbb64a926971b9e38ffc118a0315f
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-13 17:46:53 +0300

    to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const firstArg = parseInt(args[0]);
-
-if (!isNaN(firstArg)) {
-  console.log(`My number: ${firstArg}`);
-} else {
-  console.log("Not a number");
-}
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+    console.log('Not a number');
+  } else {
+    console.log('My number:', parseInt(process.argv[2]));
+  }
