b73a355e54a640138b14c45ff2d47c509f48b6a3
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-17 15:49:43 -0700

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
 """Module that lists all states from mySQL database"""
+
 import sys
 import MySQLdb
 
-def list_states (username, password, database):
-    """lists all states from the database hbtn_0e_0_usa.
-    Ags:
-        username: mysql username
-        password: mysql password
-        database: mysql database
+def list_states(username, password, database):
+    """
+    Lists all states from the database hbtn_0e_0_usa.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): MySQL database name.
     """
     # Connect to the MySQL server
-    connection  = MySQLdb.connect(host='localhost',\
-            port=3306,\
-            user=username,\
-            passwd=password,\
-            db=database)
-    c = connection.cursor()
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = connection.cursor()
 
-    c.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    states = c.fetchall()
+    states = cursor.fetchall()
 
     for state in states:
         print(state)
-    
+
     connection.close()
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
