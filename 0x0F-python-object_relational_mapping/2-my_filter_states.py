231c44f5fa0f6c6e74ff7d8babd57b6c8090898e
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-18 03:57:43 -0700

    filter_state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+Module that retrieves value of an argument
+from a MYSQL database
+"""
+
+import sys
+import MySQLdb
+
+
+def retrieve_statevalue(username, password, database, statename):
+    """
+    retrieves information of a state from the table
+    """
+    connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            state=statename
+            )
+
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        connection.close()
+
+
+if __name__ == '__main__':
+    """
+    Main function to execute the script.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    statename = sys.argv[4]
+
+    retrieve_statevalue(username, password, database, statename)
