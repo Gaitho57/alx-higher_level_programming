c42f0604a7691e0c4dfd8011873f53dbab7462c9
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-18 05:31:15 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
 
-"""
-Module that retrieves value of an argument
-from a MySQL database.
-"""
+"""Module gives lists all states from the hbtn_0e_0_usa database."""
 
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
 
-def retrieve_state_value(username, password, database, state_name):
-    """
-    Retrieves information of a state from the table.
-    """
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-    states = cursor.fetchall()
+    # Execute the SQL query to retrieve states with the specified name
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
 
-    for state in states:
-        print(state)
-
-    cursor.close()
-    connection.close()
-
-
-if __name__ == '__main__':
-    """
-    Main function to execute the script.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    retrieve_state_value(username, password, database, state_name)
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
