3e9edd08acc28bc2428ea065e9017674d1f9c8cd
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-18 05:03:59 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 
 """
-Module that retrieves value of an argument
-from a MySQL database
+This module retrieves the value of an argument from a MySQL database.
+
+Usage:
+    python3 3-select_states.py [username] [password] [database] [state name]
+
+Arguments:
+    username -- The MySQL username.
+    password -- The MySQL password.
+    database -- The name of the database.
+    state name -- The name of the state to search for.
+
+Requirements:
+    - The module uses the MySQLdb library (import MySQLdb).
+    - It connects to a MySQL server running on localhost at port 3306.
+    - The SQL query is created using the provided user input with.
+    - The results are sorted in ascending order by states.id.
+    - The retrieved values are displayed as they are in the example output.
+    - The script is not executed when imported.
+
+Example:
+    $ python3 3-select_states.py root password mydatabase "New York"
+    (1, 'New York')
 """
 
 import sys
@@ -11,7 +31,13 @@ import MySQLdb
 
 def retrieve_statevalue(username, password, database, statename):
     """
-    retrieves information of a state from the table
+    Retrieves information of a state from the table.
+
+    Arguments:
+        username -- The MySQL username.
+        password -- The MySQL password.
+        database -- The name of the database.
+        state_name -- The name of the state to search for.
     """
     connection = MySQLdb.connect(
         host='localhost',
