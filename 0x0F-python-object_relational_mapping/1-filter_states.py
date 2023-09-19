e402cdead52805e8fe41b1d2f95d87a51259db2c
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-18 23:49:59 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 """
-Module to retrieve states from a MySQL database.
+This module provides functions to retrieve states from a MySQL database.
+
+It includes a function 'retrieve_states'
+that retrieve states with names starting with 'N'
+from a MySQL database and print them to the console.
+
+Usage:
+    $ python my_module.py <username> <password> <database>
 """
 
 import MySQLdb
@@ -10,7 +17,18 @@ import sys
 
 def retrieve_states(username, password, database):
     """
-    Retrieve states with names starting with 'N' from database.
+    Retrieve states with names starting with 'N'
+    from the specified MySQL database.
+
+    Args:
+        username (str): The username to connect to the database.
+        password (str): The password for the database user.
+        database (str): The name of the database to connect to.
+
+    Returns:
+        None
+
+    Prints the retrieved states to the console.
     """
     connection = MySQLdb.connect(
         host='localhost',
@@ -34,6 +52,9 @@ def retrieve_states(username, password, database):
 if __name__ == '__main__':
     """
     Main function to execute the script.
+
+    Usage:
+        $ python my_module.py <username> <password> <database>
     """
     username = sys.argv[1]
     password = sys.argv[2]
