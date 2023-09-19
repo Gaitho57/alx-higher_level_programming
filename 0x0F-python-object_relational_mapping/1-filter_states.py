0abd882f9ffcbf15bcde941d179be77ffa5a1cd2
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-19 00:23:51 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,61 +1,14 @@
 #!/usr/bin/python3
-"""
-This module provides functions to retrieve states from a MySQL database.
-
-It includes a function 'retrieve_states'
-that retrieve states with names starting with 'N'
-from a MySQL database and print them to the console.
-
-Usage:
-    $ python my_module.py <username> <password> <database>
-"""
+"""Module lists all states from the hbtn_0e_0_usa database."""
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-def retrieve_states(username, password, database):
-    """
-    Retrieve states with names starting with 'N'
-    from the specified MySQL database.
-
-    Args:
-        username (str): The username to connect to the database.
-        password (str): The password for the database user.
-        database (str): The name of the database to connect to.
-
-    Returns:
-        None
-
-    Prints the retrieved states to the console.
-    """
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database)
-
-    c = connection.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    c.execute(query)
-    states = c.fetchall()
-
-    for state in states:
-        print(state)
-
-    c.close()
-    connection.close()
-
-
-if __name__ == '__main__':
-    """
-    Main function to execute the script.
-
-    Usage:
-        $ python my_module.py <username> <password> <database>
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    retrieve_states(username, password, database)
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
