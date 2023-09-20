7686bd584a90da7e958cd1e0b33f032dfee1af23
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-20 02:00:09 -0700

    filter

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Module that lists all cities in the specified state from the hbtn_0e_0_usa database.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get MySQL credentials and state name from command-line arguments
+    # and connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve cities in the specified state
+    query = ("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    cursor.execute(query)
+
+    # Fetch all rows and filter cities by the specified state
+    # and print the cities separated by commas
+    print(", ".join([city[2] for city in cursor.fetchall() if city[4] == sys.argv[4]]))
