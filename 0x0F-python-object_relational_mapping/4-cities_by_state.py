2de5eb9221f2017b99efbf90641384b1db118ca8
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-20 01:46:38 -0700

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 
 """
-This script lists all cities from the database hbtn_0e_4_usa
+This script lists all cities from the database hbtn_0e_4_usa.
+
+Usage: python3 script_name.py username password database
+Example: python3 script_name.py myuser mypassword mydatabase
+
+Requirements:
+- The script should connect to a MySQL server running on localhost at port
+    3306.
+- Results must be sorted in ascending order by cities.id.
+- The module MySQLdb is used for database connection (import MySQLdb).
+- The execute() method is used only once.
+- Results are displayed one row at a time.
+
 """
 
 import sys
 import MySQLdb
 
-
 def list_cities(username, password, database):
     """
-    This function lists cities from the database
+    Connects to the MySQL database and lists all cities.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): MySQL database name.
+
+    Returns:
+        None. Prints the cities directly.
+
     """
     connection = MySQLdb.connect(
         host='localhost',
@@ -34,11 +54,7 @@ def list_cities(username, password, database):
     for city in cities:
         print(city)
 
-
 if __name__ == '__main__':
-    """
-    main function where the code is executed
-    """
     if len(sys.argv) != 4:
         print("Error: Invalid arguments")
         sys.exit(1)
