a4a40cec560df5538692e0ca3f2d16883b871083
Gaitho57 <michaelgaitho10@gmail.com>
2023-10-02 13:31:00 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""
+Fetches the status of https://alx-intranet.hbtn.io/status using urllib.
+
+Prints the body response including the type, content in bytes, and UTF-8 content.
+"""
 import urllib.request
 
 def fetch_status():
     """
-    Fetches the status of https://alx-intranet.hbtn.io/status using urllib.
-
-    Prints the body response including the type, content in bytes, and UTF-8 content.
+    Fetches the status of the URL and prints the body response.
     """
     url = 'https://alx-intranet.hbtn.io/status'
 
@@ -17,4 +20,5 @@ def fetch_status():
     print("\t- content:", body)
     print("\t- utf8 content:", body.decode('utf-8'))
 
-fetch_status()
+if __name__ == "__main__":
+    fetch_status()
