6664b8196cfc2de03becf7356a3c8d5e9dd62737
Gaitho57 <michaelgaitho10@gmail.com>
2023-10-02 14:04:41 -0700

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,12 @@
 """A script that Sends a request to a given URL and displays the response body """
 import sys
 import requests
+
+
 if __name__ == "__main__":
+    """
+    where program runs
+    """
     url = sys.argv[1]
     r = requests.get(url)
     if r.status_code >= 400:
