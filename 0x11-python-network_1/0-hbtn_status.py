bbead4ed78d2d924fdc7dcfa18ccd35c3c7a7ef1
Gaitho57 <122827143+Gaitho57@users.noreply.github.com>
2023-10-02 14:45:33 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""A script fetches https://alx-intranet.hbtn.io/status.
-"""
+"""Script fetches https://alx-intranet.hbtn.io using urllib"""
 import urllib.request
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+
+
+def fetch_status():
+    """fetchs status of the URL & sisplays the response body"""
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == '__main__':
+    fetch_status()
